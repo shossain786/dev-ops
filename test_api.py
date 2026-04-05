d5a7c5ef9259c33c9db8ad23cc906b105dc3034a
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-04-05 14:36:05 +0530

    api code added

---
+++ test_api.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://payment.free.beeceptor.com/agreement-payment/debit-account-payment"
+
+data = {
+    "amount": 1000,
+    "account": "12345"
+}
+
+response = requests.post(url, json=data)
+
+print(response.status_code)
+print(response.text)
